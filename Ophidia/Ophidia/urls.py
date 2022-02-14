9d7286fc3ad863f682c3b1704523cd3b988768a4
Mustaq Hussain <mustaqhussain267@gmail.com>
2022-02-14 09:49:25 +0000

    Prototype Social Feed created

--- Ophidia/Ophidia/urls.py
+++ Ophidia/Ophidia/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
     path('accounts/', include('allauth.urls')),
+    path('social/', include('social.urls')),
 ]
