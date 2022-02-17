d2d7a8cfde5c67798dff7d237fd70146fc2b3166
Mustaq Hussain <mustaqhussain267@gmail.com>
2022-02-17 16:00:02 +0000

    Added following functionality

--- Ophidia/social/views.py
+++ Ophidia/social/views.py
@@ -1,6 +1,7 @@
+from ast import Num
 from dataclasses import fields
 from re import template
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
@@ -112,10 +113,25 @@ class ProfileView(View):
         user = profile.user
         posts = Post.objects.filter(author = user).order_by('-created_on')
 
+        followers = profile.followers.all()
+        number_of_followers = len(followers)
+
+        if number_of_followers == 0:
+            is_following = False
+
+        for follower in followers:
+            if follower == request.user:
+                is_following = True
+                break
+            else:
+                is_following = False
+
         context = {
             'user' : user,
             'profile' : profile,
             'posts' : posts,
+            'number_of_followers' : number_of_followers,
+            'is_following' : is_following,
         }
 
         return render(request, 'social/profile.html', context)
@@ -132,3 +148,17 @@ class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         profile = self.get_object()
         return self.request.user == profile.user
+
+class AddFollower(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk = pk)
+        profile.followers.add(request.user)
+
+        return redirect('profile', pk = profile.pk)
+
+class RemoveFollower(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk = pk)
+        profile.followers.remove(request.user)
+
+        return redirect('profile', pk = profile.pk)
